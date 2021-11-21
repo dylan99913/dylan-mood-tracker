30cc1f9b38e6618a6be02d20bbea0e8883f8228d
dylan99913 <dylan_wee@live.com>
2021-11-21 09:34:38 +0000

    new

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+import entry from '$lib/entry.svelte';
+import greeting from '$lib/greeting.svelte';
+import navbar from '$lib/navbar.svelte';
+</script>
+
+<!-- Entries -->
+<section class="container px-4 py-3">
+    <div class="d-flex justify-content-between">
+   	 <div class="p-2">Mood Log</div>
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+    </div>
+
+    <div class="list-group mb-3">
+   	 <!-- Individual Entries -->
+    	<Entry />
+   	 
+    </div>
+</section>
