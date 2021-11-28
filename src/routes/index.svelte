1fd44407d3cf68f2d0016e00dcbe869bb61fd573
dylan99913 <dylan_wee@live.com>
2021-11-28 09:20:49 +0000

    eh

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,18 @@
 <script>
 import Entry from '$lib/Entry.svelte';
 import Greeting from '$lib/Greeting.svelte';
+import  EntryModal from '$lib/entrymodal.svelte';
 </script>
 
  <Greeting />
 
+ <EntryModal />
+ 
 <!-- Entries -->
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#newEntry" />
     </div>
 
     <div class="list-group mb-3">
